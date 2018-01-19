41c0069c4a897f3f53504e4b1561a82c70e1762e
Juliette Rapala <jrapala@gmail.com>
2018-01-19 14:19:26 -0600

    Adding placeholder if no articles exist. Also fixing page reload after scrape is complete.

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -7,30 +7,6 @@ $(document).ready(function() {
   // Event listener for scraping new articles
   $(document).on("click", ".scrape-new", handleArticleScrape);
 
-  // MAKE THIS WORK WITH HANDLEBARS
-
-  // HTML message if there are no articles to view. 
-  function renderEmpty() {
-    var emptyAlert = $(
-      [
-        "<div class='alert alert-warning text-center'>",
-        "<h4>Uh Oh. Looks like we don't have any new articles.</h4>",
-        "</div>",
-        "<div class='panel panel-default'>",
-        "<div class='panel-heading text-center'>",
-        "<h3>What Would You Like To Do?</h3>",
-        "</div>",
-        "<div class='panel-body text-center'>",
-        "<h4><a class='scrape-new'>Try Scraping New Articles</a></h4>",
-        "<h4><a href='/saved'>Go to Saved Articles</a></h4>",
-        "</div>",
-        "</div>"
-      ].join("")
-    );
-    // Appending this data to the page
-    articleContainer.append(emptyAlert);
-  }
-
   // When user wants to save an article, update the save state of the article by using the ID data attribute
   function handleArticleSave() {
     var articleToSave = $(this).parents(".panel").data();
@@ -55,16 +31,9 @@ $(document).ready(function() {
       url: "/api/scrape"
     })
     .then(function(res) {
-      // Rerender articles and display modal displaying how many articles were scraped.
-      bootbox.alert("<h3 class='text-center m-top-80'>" + res + "<h3>");
+      // Rerender articles and display modal displaying how many articles were scraped, then reload page.
+      bootbox.alert("<h3 class='text-center m-top-80 scrape-message'>" + res + "<h3>", function(){location.reload();});
     })
-    // Reload page
-    .then(function() {
-      $.ajax({
-        method: "GET",
-        url: "/"
-      })
-    });
-  }
+  };
 
 });
